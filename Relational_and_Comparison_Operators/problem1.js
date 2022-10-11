569c08692d299ff02f2071ff7372f21ea0410abf
Tusar Mondal <tusharmondalbstma@gmail.com>
2022-10-11 19:35:20 +0530

    Relational and Comparison Operators

---
+++ Relational_and_Comparison_Operators/problem1.js
@@ -0,0 +1,11 @@
+// Rahul went to Big Bazar, he purchased items worth 4000, then he saw that there is a deal, If you
+
+ price = 3000;
+
+if(price > 4999)
+{
+  console.log("Eligible for discount");
+}
+else{
+  console.log("No Discount(false)");
+}
